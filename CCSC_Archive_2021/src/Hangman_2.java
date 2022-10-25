f12b0c330b89993f55a52d11343ce0aa5be4dd3a
Jordan Hinson <jhinson2012@gmail.com>
2022-10-25 10:06:31 -0400

    Completed hangman

--- CCSC_Archive_2021/src/Hangman_2.java
+++ CCSC_Archive_2021/src/Hangman_2.java
@@ -1,3 +1,19 @@
+/*
+Sample Input:
+3
+COMPUTER
+E R C M O Q P U Y T
+COMPUTER
+A B C D E F
+SCIENCE
+Z Y E C I S N
+
+Output:
+Case #1: Congrats, you won on COMPUTER in 10 tries
+Case #2: Sorry, you lost on COMPUTER in 6 tries C_____E_
+Case #3: Congrats, you won on SCIENCE in 7 tries
+ */
+
 import java.util.*;
 
 public class Hangman_2 {
@@ -5,15 +21,21 @@ public class Hangman_2 {
 
         Scanner input = new Scanner(System.in);
 
-        int cases = input.nextInt();
-        input.nextLine();
-        System.out.println();
+        int cases = Integer.parseInt(input.nextLine());
+
+        ArrayList<String> words = new ArrayList<>();
+        ArrayList<String> guesses = new ArrayList<>();
+
+        for (int i = 0; i <cases; i++) {
+            words.add(input.nextLine());
+            guesses.add(input.nextLine());
+        }
 
         for(int i = 0; i < cases; i++) {
 
-            String word = input.nextLine();
-            boolean test = input.hasNextLine();
-            String guess = input.next();
+            String word = words.get(i);
+            String guess = guesses.get(i);
+
             ArrayList<Character> correctLetters = new ArrayList<>();
 
             int attempts = 0;
@@ -35,21 +57,20 @@ public class Hangman_2 {
             }
 
             if(wrong < 4) {
-                System.out.println("Case #" + i + ": Congrats, you won on" + word + " in " + attempts + " tries");
+                System.out.println("Case #" + i + ": Congrats, you won on " + word + " in " + attempts + " tries");
             }
             else {
-                String word_ = "";
-                for(int j = 0; j < word.length(); j++) {
-                    word_ += "_";
+                String word_ = word.toLowerCase();
+
+                for(int j = 0; j< word_.length(); j++) {
+                    if(correctLetters.contains((word_.charAt(j) + "").toUpperCase().charAt(0))) {
+                        word_ = word_.substring(0,j) + (word_.charAt(j) + "").toUpperCase() + word_.substring(j+ 1);
+                    }
                 }
 
-                for(int j = 0; j < word.length(); j++) {
-                    for (int k = 0; k < correctLetters.size(); k++) {
-                        if(word.charAt(j) == correctLetters.indexOf(k)) {
-                            char[] chararray = word_.toCharArray();
-                            chararray[j] = (word.charAt(j) + "").toUpperCase().charAt(0);
-                            word_ = chararray.toString();
-                        }
+                for(int j = 0; j< word_.length(); j++) {
+                    if(Character.isLowerCase(word_.charAt(j))) {
+                        word_ = word_.substring(0,j) + "_" + word_.substring(j+1);
                     }
                 }
 
